e4b64638c4b22bdcb7daf94cd4cb0e6a1adf2ef8
Jon Ball <jonathon.ball@gmail.com>
2019-05-23 23:38:59 -0400

    updated front page

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,99 +1,92 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+      html, body {
+          background-color: #fff;
+          font-family: 'Permanent Marker', cursive;
+          font-size: 48px;
+          height: 100vh;
+          margin: 0;
+      }
 
-            .full-height {
-                height: 100vh;
-            }
+      .full-height {
+          height: 100vh;
+      }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      .flex-center {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+      }
 
-            .position-ref {
-                position: relative;
-            }
+      .position-ref {
+          position: relative;
+      }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+      .top-right {
+          position: absolute;
+          right: 10px;
+          top: 18px;
+          padding-right: 50px;
+      }
 
-            .content {
-                text-align: center;
-            }
+      .content {
+          text-align: center;
+      }
 
-            .title {
-                font-size: 84px;
-            }
+      .links > a {
+          color: #000;
+          padding: 20px;
+          text-decoration: none;
+      }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+      .logo {
+          color: #636b6f;
+          height: 64px;
+          width: 64px;
+      }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+      .m-b-md {
+          margin-bottom: 30px;
+      }
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+      .naked-list {
+          list-style: none;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="flex-center position-ref full-height">
+      <div class="top-right links">
+        <ul class="naked-list">
+          <li>
+            <a href="https://www.instagram.com/jonathonball/"><img class="logo" src="{{ asset('images/glyph-logo_May2016.png') }}"/></a>
+          </li>
+          <li>
+            <a href="https://github.com/jonathonball"><img class="logo" src="{{ asset('images/GitHub-Mark-120px-plus.png') }}"/></a>
+          </li>
+        </ul>
+      </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      <div class="content">
+        <div class="m-b-md">
+          <img src="{{ asset('images/carlton.gif') }}" title="I love this gif.  It's a lovely embodiment of a beautiful man expressing joy "/>
         </div>
-    </body>
+
+        <div class="links">
+          <a href="#">New Content Coming Soon</a>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
