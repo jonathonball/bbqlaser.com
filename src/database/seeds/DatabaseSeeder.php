c90e30c5dc7d720b47a02245c32bb86e3c88cc44
Jon Ball <jonathon.ball@gmail.com>
2019-05-27 21:11:23 -0400

    removed extra seeder

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PlatformsTableSeeder::class);
-        $this->call(GamesTableSeeder::class);
+        $this->call(GamePlatformTableSeeder::class);
     }
 }
