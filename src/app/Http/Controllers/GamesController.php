efd83712d3a78e223805b4d598e488e03e3a2d7b
Jon Ball <jonathon.ball@gmail.com>
2019-06-24 20:28:04 -0400

    changed formatting

--- src/app/Http/Controllers/GamesController.php
+++ src/app/Http/Controllers/GamesController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Game;
+use App\Services\SteamApi;
 
 class GamesController extends Controller
 {
+
     public function index() {
       $games = Game::all();
-      return view('games', ['games' => $games]);
+      return view('games', [
+          'games' => $games,
+      ]);
     }
+
 }
