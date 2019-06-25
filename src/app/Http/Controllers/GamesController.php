e0f40c82a2c9cb0859757c1897eed68ca031f93d
Jon Ball <jonathon.ball@gmail.com>
2019-06-24 22:26:44 -0400

    added end-point for refreshing list

--- src/app/Http/Controllers/GamesController.php
+++ src/app/Http/Controllers/GamesController.php
@@ -9,11 +9,24 @@ use App\Services\SteamApi;
 class GamesController extends Controller
 {
 
-    public function index() {
-      $games = Game::all();
-      return view('games', [
-          'games' => $games,
-      ]);
+    public function index(SteamApi $steam)
+    {
+        $games = Game::all();
+
+        return view('games', [
+            'games' => $games,
+        ]);
+    }
+
+    public function updateSteamGames(SteamApi $steam)
+    {
+        $gameData = $steam->getOwnedGames();
+        $gameCount = $results['response']['game_count'];
+        $games = $results['response']['games'];
+        // TODO add new games to database
+        return view('api-result', [
+            'count' => $gameCount,
+        ]);
     }
 
 }
