32111136eca09e3ee2fad2d65b7441eae785e0a8
Jon Ball <jonathon.ball@gmail.com>
2019-06-24 22:27:08 -0400

    removed experimental game seeder

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(GamePlatformTableSeeder::class);
+        // $this->call(GamePlatformTableSeeder::class);
         $this->call(ImageLinkSeeder::class);
     }
 }
